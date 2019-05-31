f30732a25e9a1b7e07665d2e312f4acf9d9b3777
= <kieranleep@hotmail.com>
2019-05-31 18:25:45 +0100

    Created more parsers

--- discord_cli/parsers.py
+++ discord_cli/parsers.py
@@ -1,6 +1,8 @@
 import discord_cli.validation as validation
 import discord_cli.exceptions as exceptions
 
+from datetime import datetime
+
 class Base_Parser(object):
     
     def __init__(self):
@@ -12,7 +14,7 @@ class Base_Parser(object):
 class Integer_Parser(Base_Parser):
     
     def __init__(self, min, max, include_min, include_max):
-        super(Integer_Parser, self).__init__()        
+        super(Integer_Parser, self).__init__()
         self._min = min
         self._max = max
         self._include_min = include_min
@@ -20,9 +22,9 @@ class Integer_Parser(Base_Parser):
     
     async def parse(self, input_string):
 
-        validation.validate_integer(input_string)
+        await validation.async_validate_integer(input_string)
         result = int(input_string)
-        validation.validate_bounds(result, self._min, self._max, self._include_min, self._include_max)
+        await validation.async_validate_bounds(result, self._min, self._max, self._include_min, self._include_max)
         return result
     
 class Word_Parser(Base_Parser):
@@ -35,9 +37,138 @@ class Word_Parser(Base_Parser):
         self._include_max_length = include_max_length
     
     async def parse(self, input_string):
-        validation.validate_word(input_string)
+        await validation.async_validate_word(input_string)
         try:
-            validation.validate_bounds(len(input_string), self._min_length, self._max_length, self._include_min_length, self._include_max_length)
+            await validation.async_validate_bounds(len(input_string), self._min_length, self._max_length, self._include_min_length, self._include_max_length)
         except exceptions.Discord_CLI_Error as e:
             raise type(e)('length {}'.format(str(e)))
         return input_string
+
+# Float
+class Float_Parser(Base_Parser):
+
+    def __init__(self, min, max, include_min, include_max):
+        super(Float_Parser, self).__init__()
+        self._min = min
+        self._max = max
+        self._include_min = include_min
+        self._include_max = include_max
+    
+    async def parse(self, input_string):
+        await validation.async_validate_float(input_string)
+        result = float(input_string)
+        await validation.async_validate_bounds(result, self._min, self._max, self._include_min, self._include_max)
+        return result
+
+# String
+class String_Parser(Base_Parser):
+
+    def __init__(self, min_length, max_length, include_min_length, include_max_length):
+        super(String_Parser, self).__init__()
+        self._min_length = min_length
+        self._max_length = max_length
+        self._include_min_length = include_min_length
+        self._include_max_length = include_max_length
+    
+    async def parse(self, input_string):
+        await validation.async_validate_string(input_string)
+        try:
+            await validation.async_validate_bounds(len(input_string), self._min_length, self._max_length, self._include_min_length, self._include_max_length)
+        except exceptions.Discord_CLI_Error as e:
+            raise type(e)('length {}'.format(str(e)))
+        return input_string
+
+# User Mention
+# Without nickname      <@95584437231689728>
+# With nickname         <@!95584437231689728>
+class User_Mention_Parser(Base_Parser):
+
+    def __init__(self):
+        super(User_Mention_Parser, self).__init__()
+    
+    async def parse(self, input_string):
+        await validation.async_validate_user_mention(input_string)
+        for c in '<@!>':
+            input_string = input_string.replace(c, '')
+        return int(input_string)
+
+# Channel Mention
+# <#291649024409468928>
+
+class Channel_Mention_Parser(Base_Parser):
+
+    def __init__(self):
+        super(Channel_Mention_Parser, self).__init__()
+    
+    async def parse(self, input_string):
+        await validation.async_validate_channel_mention(input_string)
+        for c in '<#>':
+            input_string = input_string.replace(c, '')
+        return int(input_string)
+
+# Role Mention
+# <@&357235474869387276>
+class Role_Mention_Parser(Base_Parser):
+
+    def __init__(self):
+        super(Role_Mention_Parser, self).__init__()
+    
+    async def parse(self, input_string):
+        await validation.async_validate_role_mention(input_string)
+        for c in '<@&>':
+            input_string = input_string.replace(c, '')
+        return int(input_string)
+
+# Date
+# In the format %d/%m/%Y for now, may become customizable in future
+class Date_Parser(Base_Parser):
+    
+    def __init__(self):
+        super(Date_Parser, self).__init__()
+    
+    async def parse(self, input_string):
+        await validation.async_validate_date(input_string)
+        return datetime.strftime(input_string, '%d/%m/%Y')
+
+# Time
+# In the format %H:%M:%S for now, may become customizable in future
+class Time_Parser(Base_Parser):
+
+    def __init__(self):
+        super(Time_Parser, self).__init__()
+    
+    async def parse(self, input_string):
+        await validation.async_validate_time(input_string)
+        return datetime.srtftime(input_string, '%H:%M:%S')
+
+# DateTime
+# In the format %H:%M:%S-%d/%m/%Y for now, may become customizable in future
+class Datetime_Parser(Base_Parser):
+
+    def __init__(self):
+        super(Datetime_Parser, self).__init__()
+
+    async def parse(self, input_string):
+        await validation.async_validate_datetime(input_string)
+        return datetime.strftime(input_string, '%H:%M:%S-%d/%m/%Y')
+
+# Enum
+class Enum_Parser(Base_Parser):
+
+    def __init__(self, values):
+        super(Enum_Parser, self).__init__()
+        if not isinstance(values, list):
+            raise exceptions.Type_Error('Enum values expected list instance, {} found'.format(values.__class__.__name__))
+        if len(values) == 0:
+            raise exceptions.Value_Error('Enum values must not have length 0')
+        try:
+            for string in values:
+                validation.validate_word(string)
+        except exceptions.Discord_CLI_Error as e:
+            raise type(e)('Enum values elements ' + str(e))
+        self._values = values
+    
+    async def parse(self, input_string):
+        await validation.async_validate_string(input_string)
+        if input_string not in self._values:
+            raise exceptions.Value_Error('must be element in {}'.format(str(self._values)))
--- discord_cli/validation.py
+++ discord_cli/validation.py
@@ -15,11 +15,20 @@ def validate_command_name(string):
 def validate_word(string):
     validate_string(string)
     if re.match('.*[^a-zA-Z].*', string):
-        raise exceptions.Value_Error('must only contain letters')
+        raise exceptions.Value_Error('must represent an integer')
+
+def validate_float(string):
+    validate_string(string)
+    try:
+        float(string)
+    except ValueError:
+        raise exceptions.Value_Error('must represent a float')
 
 def validate_integer(string):
     validate_string(string)
-    if re.match('.*[^0-9].*', string):
+    try:
+        int(string)
+    except ValueError:
         raise exceptions.Value_Error('must only contain digits')
 
 def validate_letter(string):
@@ -39,3 +48,113 @@ def validate_bounds(value, min, max, include_min, inlucde_max):
             raise exceptions.Value_Error('must be less than {}'.format(min))
         if result > max:
             raise exceptions.Value_Error('cannot be greater than {}'.format(max))
+
+def validate_user_mention(string):
+    validate_string(string)
+    if not re.match('<@!?\d+>', string):
+        raise exceptions.Value_Error('must represent a user mention')
+
+def validate_channel_mention(string):
+    validate_string(string)
+    if not re.match('<#\d+>', string):
+        raise exceptions.Value_Error('must represent a channel mention')
+
+def validate_role_mention(string):
+    validate_string(string)
+    if not re.match('<@&\d+>', string):
+        raise exceptions.Value_Error('must represent a role mention')
+
+def validate_date(string):
+    validate_string(string)
+    if not re.match('\d\d/\d\d/\d\d', string):
+        raise exceptions.Value_Error('must represent a date')
+
+def validate_time(string):
+    validate_string(string)
+    if not re.match('\d\d:\d\d:\d\d', string):
+        raise exceptions.Value_Error('must represent a time')
+
+def validate_date_time(string):
+    validate_string(string)
+    if not re.match('\d\d:\d\d:\d\d-\d\d/\d\d/\d\d\d\d', string):
+        raise exceptions.Value_Error('must represent a datetime')
+
+# Async counterparts
+
+async def async_validate_string(string):
+    if not isinstance(string, str):
+        raise exceptions.Type_Error('expected str instance, {} found'.format(string.__class__.__name__))
+    if len(string) == 0:
+        raise exceptions.Value_Error('must not have 0 length')
+
+async def async_validate_command_name(string):
+    await async_validate_string(string)
+    if re.match('.*[^a-zA-Z_].*', string):
+        raise exceptions.Value_Error('must only contain letters and underscores')
+
+async def async_validate_word(string):
+    await async_validate_string(string)
+    if re.match('.*[^a-zA-Z].*', string):
+        raise exceptions.Value_Error('must represent an integer')
+
+async def async_validate_float(string):
+    await async_validate_string(string)
+    try:
+        float(string)
+    except ValueError:
+        raise exceptions.Value_Error('must represent a float')
+
+async def async_validate_integer(string):
+    await async_validate_string(string)
+    try:
+        int(string)
+    except ValueError:
+        raise exceptions.Value_Error('must only contain digits')
+
+async def async_validate_letter(string):
+    await async_validate_word(string)
+    if len(string) > 1:
+        raise exceptions.Value_Error('must have length 1')
+
+async def async_validate_bounds(value, min, max, include_min, inlucde_max):
+    if min is not None:
+        if include_min == False and result <= min:
+            raise exceptions.Value_Error('must be greater than {}'.format(min))
+        if result < min:
+            raise exceptions.Value_Error('cannot be less than {}'.format(min))
+    
+    if max is not None:
+        if include_max == False and result >= max:
+            raise exceptions.Value_Error('must be less than {}'.format(min))
+        if result > max:
+            raise exceptions.Value_Error('cannot be greater than {}'.format(max))
+
+async def async_validate_user_mention(string):
+    await async_validate_string(string)
+    if not re.match('<@!?\d+>', string):
+        raise exceptions.Value_Error('must represent a user mention')
+
+async def async_validate_channel_mention(string):
+    await async_validate_string(string)
+    if not re.match('<#\d+>', string):
+        raise exceptions.Value_Error('must represent a channel mention')
+
+async def async_validate_role_mention(string):
+    await async_validate_string(string)
+    if not re.match('<@&\d+>', string):
+        raise exceptions.Value_Error('must represent a role mention')
+
+async def async_validate_date(string):
+    await async_validate_string(string)
+    if not re.match('\d\d/\d\d/\d\d', string):
+        raise exceptions.Value_Error('must represent a date')
+
+async def async_validate_time(string):
+    await async_validate_string(string)
+    if not re.match('\d\d:\d\d:\d\d', string):
+        raise exceptions.Value_Error('must represent a time')
+
+async def async_validate_date_time(string):
+    await async_validate_string(string)
+    if not re.match('\d\d:\d\d:\d\d-\d\d/\d\d/\d\d\d\d', string):
+        raise exceptions.Value_Error('must represent a datetime')
